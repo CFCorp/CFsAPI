7714fd708e9b25e86a1ffac7e03064b5b07c9a57
CFTheMaster <hentaimasterpad@gmail.com>
2018-05-30 02:35:00 +0200

    add some WIP stuff

---
+++ src/AppBundle/Controller/extraStatsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class extraStatsController extends Controller
+{
+    /**
+     * @Route("/stats", name="stats")
+     */
+    public function indexAction($name)
+    {
+        return $this->render('default/stats.html.twig', null);
+    }
+}
