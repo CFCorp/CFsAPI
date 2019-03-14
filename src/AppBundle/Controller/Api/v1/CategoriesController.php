88f0722cee641a25d806887a5a1bac616585e81b
computerfreaker <hentaimasterpad@gmail.com>
2019-03-14 10:43:23 +0100

    make categories usefull

--- src/AppBundle/Controller/Api/v1/CategoriesController.php
+++ src/AppBundle/Controller/Api/v1/CategoriesController.php
@@ -6,16 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CategoriesController extends Controller
 {
+
+    public function categoryNameUrl($name, Request $request){
+        $full_url = $request->getHost().$this->generateUrl("$name");
+        return $full_url;
+    }
+
     /**
      * @Route("/v1/categories", name="categories")
      * @Method("GET")
      * @param $id
      */
-    public function getAction()
+    public function getAction(Request $request)
     {
         $response = new Response();
         $response->headers->set('Content-Type', 'application/json; charset=UTF-8');
@@ -23,6 +30,15 @@ class CategoriesController extends Controller
         $response->headers->set('Cache-Control','no-cache');
         $response->send();
 
-        return new JsonResponse(array("anime", "baguette", "dva", "hentai", "hug", "trap", "nsfwneko", "neko", "yuri"));
+        return new JsonResponse(array(
+            "anime" => $this->categoryNameUrl("anime", $request),
+            "baguette" => $this->categoryNameUrl("baguette", $request),
+            "dva" => $this->categoryNameUrl("dva", $request),
+            "hentai" => $this->categoryNameUrl("hentai", $request),
+            "hug" => $this->categoryNameUrl("hug", $request),
+            "trap" => $this->categoryNameUrl("trap", $request),
+            "nsfwneko" => $this->categoryNameUrl("nsfwneko", $request),
+            "neko" => $this->categoryNameUrl("neko", $request),
+            "yuri" => $this->categoryNameUrl("yuri", $request)));
     }
 }
